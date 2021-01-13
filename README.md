2aae0339f7f2eb18634d9a99bec8f9db4894d40d
Jason Labbe <jasonlabbe@gmail.com>
2021-01-13 23:45:02 +0800

    Added installer and README

---
+++ README.md
@@ -0,0 +1,45 @@
+# Bifrost Output Reader
+
+Reads output attributes from a Bifrost graph and displays all of its data.
+
+## Limitations
+
+* Output port must have an active connection (can't create a port then unplug)
+* This tool doesn't support the following types:
+    * char2, char3, char4
+    * Matrices that aren't 4x4 and non-floating point types
+    * Enums
+    * Amino objects
+    * Locations
+    * Custom types except bool2, bool3, bool4
+    * Any 3D arrays
+
+## Installation
+
+Drag and drop the file installer.py onto a viewport to run it.
+Follow the instructions to complete the installation.
+The installer gives you a choice to install it into your maya preferences, or to pick a location.
+You can also manually install it by simply copying the `bifrost_output_reader` directory to folder where your Python path is pointing to.
+
+## Launching Tool
+
+After installation you can immediately launch the tool by executing the following Python code in the Script Editor:
+
+```
+from bifrost_output_reader import bifrost_output_reader
+bifrost_output_reader.launch()
+```
+
+## Dependencies
+
+This script has no extra dependencies.
+
+## Reporting bugs & requesting features
+
+You can send all bug reports and any feature requests to jasonlabbe@gmail.com.
+For errors please include the following to ease debugging:
+
+* Your operating system
+* Your version of Maya
+* If possible, open up the Script Editor and copy & paste the error message.
+* Include any steps that will replicate the error.
---
+++ installer.py
@@ -0,0 +1,119 @@
+"""
+Drag and drop this file into your viewport to run the installer.
+"""
+
+import sys
+import os
+import shutil
+import traceback
+import time
+
+import maya.cmds as cmds
+
+
+tool_name = "bifrost_output_reader"
+
+
+def onMayaDroppedPythonFile(*args):
+    try:
+        source_dir = os.path.dirname(__file__)
+        source_path = os.path.normpath(os.path.join(source_dir, "scripts", tool_name))
+        home_dir = os.getenv("HOME")
+        install_path = None
+
+        # Make sure this installer is relative to the main tool.
+        if not os.path.exists(source_path):
+            raise RuntimeError("Unable to find `scripts/{toolName}` relative to this installer file.".format(toolName=tool_name))
+
+        # Suggest to install in user's preferences if it exists.
+        if home_dir:
+            install_path = os.path.normpath(os.path.join(home_dir, "maya", "scripts"))
+
+            msg = (
+                "The tool will be installed in a new folder here:\n"
+                "{installPath}\n"
+                "\n"
+                "Is this ok?".format(
+                    installPath=install_path))
+
+            decision = cmds.confirmDialog(
+                title="Installation path", message=msg, icon="warning",
+                button=["OK", "Cancel", "No, let me pick another path!"],
+                cancelButton="Cancel", dismissString="Cancel")
+
+            if decision == "Cancel":
+                return
+            elif decision == "No, let me pick another path!":
+                install_path = None
+
+        # If no install path is supplied, open a file picker to choose a path.
+        if install_path is None:
+            results = cmds.fileDialog2(fileMode=3, okCaption="Install here", caption="Pick a folder to install to")
+            if not results:
+                return
+            install_path = os.path.normpath(results[0])
+
+        # Check if install path is in Python's path.
+        python_paths = [os.path.normpath(path) for path in sys.path]
+        if install_path not in python_paths:
+            msg = (
+                "Python isn't pointed to this path.\n"
+                "\n"
+                "This means Python won't be able to find the tool and run it.\n"
+                "This can be set in your Maya.env or userSetup.py files.\n"
+                "\n"
+                "Do you want to continue anyways?")
+
+            continue_anyways = cmds.confirmDialog(
+                title="Warning!", message=msg, icon="warning",
+                button=["OK", "Cancel"],
+                cancelButton="Cancel", dismissString="Cancel")
+
+            if continue_anyways == "Cancel":
+                return
+
+        # Remove directory if it already exists.
+        tool_path = os.path.join(install_path, tool_name)
+        if os.path.exists(tool_path):
+            # Give a warning first!
+            msg = ("This folder already exists:\n"
+                   "{toolPath}\n"
+                   "\n"
+                   "Continue to overwrite it?".format(toolPath=tool_path))
+
+            confirm_overwrite = cmds.confirmDialog(
+                title="Overwrite?", message=msg, icon="warning",
+                button=["OK", "Cancel"],
+                cancelButton="Cancel", dismissString="Cancel")
+
+            if confirm_overwrite == "Cancel":
+                return
+
+            shutil.rmtree(tool_path)
+
+        # Windows may throw an 'access denied' exception doing a copytree right after a rmtree, so force a delay.
+        time.sleep(1)
+        shutil.copytree(source_path, tool_path)
+
+        # Display success!
+        msg = (
+            "The tool has been successfully installed!\n"
+            "If you want to remove it then simply delete this folder:\n"
+            "{toolPath}\n\n"
+            "Run the tool from the script editor by executing the following:\n\n"
+            "from bifrost_output_reader import bifrost_output_reader\n"
+            "bifrost_output_reader.launch()".format(toolPath=tool_path))
+
+        cmds.confirmDialog(title="Success!", message=msg, button=["OK"])
+    except Exception as e:
+        print traceback.format_exc()
+
+        # Display error message if an exception was raised.
+        msg = (
+            "{err}\n"
+            "\n"
+            "If you need help or have questions please send an e-mail with subject "
+            "'{toolName} installation' to jasonlabbe@gmail.com".format(
+                err=e, toolName=tool_name))
+
+        cmds.confirmDialog(title="Installation has failed!", message=msg, icon="critical", button=["OK"])
